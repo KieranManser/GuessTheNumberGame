288e9bc43d3d7a429e8dad572bfc39666cf3c681
KieranManser <kieranmanser00@gmail.com>
2025-08-28 13:23:25 +0100

    Updated Guess the number to allow for replay based on user input after winning.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,34 +8,39 @@ public class Main {
         Then user will guess within the random range
         Then code will output if the number is too high too low or correct
         Then user will win the game after correctly guessing.
-
-        //Creating a Random number
-        Random rand = new Random();
-        int randNum = rand.nextInt(100) + 1;
         */
 
         //Scanner for user inputs
         Scanner scanner = new Scanner(System.in);
-        //declaring guess as a variable
+        //declaring the players guess as a variable
         int userGuess = 0;
+        boolean stopPlaying = false;
+        //do loop allows for the player to replay. keepPlaying variable is false. Once set to true it will carry on do loop.
+        do {
+            //Creating a Random number
+            int randomNum = (int) (Math.random() * 100 + 1);
+            System.out.println(randomNum);
+            System.out.println("I am thinking of a number!");
+            //resetting user guess as auto-win was possible on rare chance to get same number twice
+            userGuess = 0;
 
-        //Creating a Random number
-        int randomNum = (int) (Math.random() * 100 + 1);
-        System.out.println(randomNum);
-
-        //Loop to allow multiple guesses, Will loop until user inputs correct value
-        while (userGuess != randomNum) {
-            System.out.println("Guess a Number: ");
-            //User inputs a guess
-            userGuess = scanner.nextInt();
-            //Reads user input creates the win condition
-            if (userGuess == randomNum) {
-                System.out.println("You Win!");
-            } else if (randomNum > userGuess) {
-                System.out.println("Too low! Guess higher.");
-            } else {
-                System.out.println("Too high! Guess lower.");
+            //Loop to allow multiple guesses, Will loop until user inputs correct value
+            while (userGuess != randomNum) {
+                System.out.println("Guess the Number: ");
+                //User inputs a guess
+                userGuess = scanner.nextInt();
+                //Reads user input creates the win condition
+                if (userGuess == randomNum) {
+                    System.out.println("You Win!");
+                } else if (randomNum > userGuess) {
+                    System.out.println("Too low! Guess higher.");
+                } else {
+                    System.out.println("Too high! Guess lower.");
+                }
             }
-        }
+            //asks if the player wants to go again. If yes or y is answered it will reset the game. If no it exits the game.
+            System.out.println("Play again? (Yes/No)");
+            stopPlaying = scanner.next().toUpperCase().charAt(0) == 'Y';
+        } while (stopPlaying);
     }
 }
